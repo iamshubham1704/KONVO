2685084a8d3d5cc08a3ac71a48ca101b9bc1c564
iamshubham1704 <sstech514@gmail.com>
2024-12-27 16:46:30 +0530

    final

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "import.meta.env.VITE_API_KEY",
+    apiKey: "AIzaSyDeAXQZbEMZSPWbTOo4rbXddDCFymlp_dQ",
     authDomain: "konvo-2f03e.firebaseapp.com",
     projectId: "konvo-2f03e",
     storageBucket: "konvo-2f03e.firebasestorage.app",
     messagingSenderId: "55303094331",
     appId: "1:55303094331:web:82df640c3dfe9a4e6a9c1e"
-};
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
